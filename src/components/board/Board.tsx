b28cfdc39036dbc3a4d6b034cd526a3fc96448d7
Kim Brun Jørgensen <kim.joergensen@outlook.com>
2022-04-04 21:51:25 +0200

    added backrow

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -2,41 +2,54 @@ import { useState } from 'react'
 import * as styles from './board.module.scss'
 
 export const Board = () => {
-  const [playerTiles, setPlayerTiles] = useState<{ position: number }[]>([
-    { position: 1 },
-    { position: 2 },
-    { position: 3 },
-    { position: 4 },
-    { position: 5 },
-    { position: 6 },
-  ])
-
-  const [opponentTiles, setOpponentTiles] = useState<{ position: number }[]>([
-    { position: 1 },
-    { position: 2 },
-    { position: 3 },
-    { position: 4 },
-    { position: 5 },
-    { position: 6 },
-  ])
+  const [playerTiles] = useState<{ position: number }[]>(mock)
+  const [playerBackrow] = useState<{ position: number }[]>(mock)
+  const [opponentTiles] = useState<{ position: number }[]>(mock)
+  const [opponentBackrow] = useState<{ position: number }[]>(mock)
 
   return (
     <div className={styles.board}>
       <div className={styles.opponent}>
-        {opponentTiles.map((tile) => (
-          <div className={styles.tile} key={tile.position}>
-            tile {tile.position}
-          </div>
-        ))}
+        <div className={styles.hand}></div>
+
+        <div className={styles.backrow}>
+          {opponentBackrow.map((backrow) => (
+            <div className={styles.tile} key={backrow.position}>
+              backrow {backrow.position}
+            </div>
+          ))}
+        </div>
+
+        <div className={styles.field}>
+          {opponentTiles.map((tile) => (
+            <div className={styles.tile} key={tile.position}>
+              tile {tile.position}
+            </div>
+          ))}
+        </div>
       </div>
 
       <div className={styles.player}>
-        {playerTiles.map((tile) => (
-          <div className={styles.tile} key={tile.position}>
-            tile {tile.position}
-          </div>
-        ))}
+        <div className={styles.field}>
+          {opponentTiles.map((tile) => (
+            <div className={styles.tile} key={tile.position}>
+              tile {tile.position}
+            </div>
+          ))}
+        </div>
+
+        <div className={styles.backrow}>
+          {opponentBackrow.map((backrow) => (
+            <div className={styles.tile} key={backrow.position}>
+              backrow {backrow.position}
+            </div>
+          ))}
+        </div>
+
+        <div className={styles.hand}></div>
       </div>
     </div>
   )
 }
+
+const mock = [{ position: 1 }, { position: 2 }, { position: 3 }, { position: 4 }, { position: 5 }, { position: 6 }]
