60b04018f587e596bbf967c09d48fbecb7a74d78
Kim Brun Jørgensen <kim.joergensen@outlook.com>
2022-04-04 21:11:14 +0200

    game board with tiles

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -2,18 +2,41 @@ import { useState } from 'react'
 import * as styles from './board.module.scss'
 
 export const Board = () => {
-  const [tiles, setTiles] = useState<{ position: number }[]>([
-    { position: 0 },
+  const [playerTiles, setPlayerTiles] = useState<{ position: number }[]>([
     { position: 1 },
     { position: 2 },
     { position: 3 },
+    { position: 4 },
+    { position: 5 },
+    { position: 6 },
+  ])
+
+  const [opponentTiles, setOpponentTiles] = useState<{ position: number }[]>([
+    { position: 1 },
+    { position: 2 },
+    { position: 3 },
+    { position: 4 },
+    { position: 5 },
+    { position: 6 },
   ])
 
   return (
     <div className={styles.board}>
-      {tiles.map((tile) => (
-        <div key={tile.position}>tile {tile.position}</div>
-      ))}
+      <div className={styles.opponent}>
+        {opponentTiles.map((tile) => (
+          <div className={styles.tile} key={tile.position}>
+            tile {tile.position}
+          </div>
+        ))}
+      </div>
+
+      <div className={styles.player}>
+        {playerTiles.map((tile) => (
+          <div className={styles.tile} key={tile.position}>
+            tile {tile.position}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
